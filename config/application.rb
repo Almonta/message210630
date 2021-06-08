ce28065fc5a55dc6ed658732ddda984ff89726aa
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-08 09:34:27 +0900

    jQueryとBootstrapを導入、cssとJavaScriptのマニフェストファイルに読み込みを記述した

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Instagramclone
     # Initialize configuration defaults for originally generated Rails version.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
       g.assets false
