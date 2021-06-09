c51776a86b950675097579f6ec10b3e1ed0bb33a
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-09 09:23:13 +0900

    init

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(picture)
     @picture = picture
-    mail to: "tkg.8.mthr@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "tkg.8.mthr@gmail.com", subject: "投稿の確認メール"
   end
 end
